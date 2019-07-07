51ec8604ddf10c2a36ce3aadfac01da68ea1e4e6
github username <github email address>
2019-07-06 23:58:45 -0400

    Done.

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,6 +12,9 @@ class User < ActiveRecord::Base
 
     checked_out_book = Checkout.create(book_id: book.id, user_id: self.id)
     checked_out_book.checked_out = true
+    checked_out_book.user_id = self.id
+
+    #DATES
     checked_out_date = Time.now.strftime("%A, %B %d, %Y")
     checked_out_book.due_date = Time.now + (7*24*3600)
     checked_out_book.due_date.strftime("%A, %B %d, %Y")
@@ -20,9 +23,16 @@ class User < ActiveRecord::Base
     puts "Checked Out Date: #{checked_out_date}"
     puts "Due Date: #{checked_out_book.due_date}"
 
-     user =  User.find(self.id)
-     user.books.create(title: book.title)
-     user.checkouts << checked_out_book
+  end
+
+  def books
+    puts "You've checked out the following books:"
+    Checkout.all.each_with_index do |checkout, i|
+      if checkout.user_id == self.id
+        puts "#{i+1}. #{checkout.book.title}"
+      end
+    end
+    return "Enjoy your reads"
   end
 
   # def return_book(book_id, user_id)
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,46 @@
+User.destroy_all
+Book.destroy_all
+Checkout.destroy_all
+Category.destroy_all
+
+%w(Emmanuel Anna Madeline Jonathan Mijo Vida Genesis Comfort).each do |name|
+  User.create(name: name)
+end
 
 
+[
+  "Holy Bible",
+  "Chamber Of Secrets",
+  "Moby Dick",
+  "Hunger Games",
+  "Half Blood Prince",
+  "A Child Called It",
+  "Things Fall Apart",
+  "Long Walk To Freedom",
+  "The Great Gatsby",
+  "Hamlet"
+].each do |book|
+  Book.create(title: book)
+end
 
 
+[
+  "Paul the Apostle",
+  "JK Rolwing",
+  "Herman Melville",
+  "Suzanne Collins",
+  "Dave Pelzer",
+  "Chinua Achebe",
+  "Nelson Mandela",
+  "F Scott Fitzgerald",
+  "William Shakespeare"
+].each do |author|
+  Author.create(name: author)
+end
 
+
+%w(Fiction Fantasy Non-Fiction Mystery Thriller Play Science-Fiction Romance).each do |category|
+  Category.create(name: category)
+end
 
 puts "Done Seeding!!"
